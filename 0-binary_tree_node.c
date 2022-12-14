a77c41066f2639ba85169cc1358ece23173bfd92
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-14 04:40:49 -0800

    fixed compiler issue

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  * Return: NULL if failed, or pointer to the new node
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
 	binary_tree_t *new;
