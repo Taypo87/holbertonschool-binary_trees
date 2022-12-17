c371185f08c31bbbec7bbb90bce478c316becf57
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-17 12:55:31 -0800

    task 9 for binary trees

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t nodes_l = 0;
+	size_t nodes_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	nodes_l = binary_tree_height(tree->left) + 1;
+	nodes_r = binary_tree_height(tree->right) +1;
+
+	if (nodes_l > nodes_r)
+		return (nodes_l);
+	else
+		return (nodes_r);
+}
