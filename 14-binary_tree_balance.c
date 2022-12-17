40f9ae38bdba6466803c8d0fe677693f4be6233a
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-17 13:52:36 -0800

    task 14 for binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (height_l - height_r);
 }
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	height_l = binary_tree_height(tree->left) + 1;
+	height_r = binary_tree_height(tree->right) + 1;
+
+	if (height_l >= height_r)
+		return (height_l);
+
+	else
+		return (height_r);
+}
