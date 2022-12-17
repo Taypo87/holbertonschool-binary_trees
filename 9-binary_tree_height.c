b4069ed6e7f82eb3e032eb8e3ce06aa6faf7091c
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-17 12:57:14 -0800

    task 9 for binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_height - measures the height of a given binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: total height of tree
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
