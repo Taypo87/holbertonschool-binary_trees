d9218971f0b0df30580e7d72ccc33fcf5f24711b
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-17 14:23:46 -0800

    task 17 for binary trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || node->parent == NULL)
 		return (NULL);
 	if (node->parent->left != node)
-		return (node->parent_left);
+		return (node->parent->left);
 
 	return (node->parent->right);
 }
